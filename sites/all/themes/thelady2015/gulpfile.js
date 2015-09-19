ea95bea145bd73198e564a8e7546432470b0d013
Nelly Moseki <dineo06.moseki@gmail.com>
2015-09-19 14:09:49 +0200

    Created custom theme TheLady2015

---
+++ sites/all/themes/thelady2015/gulpfile.js
@@ -0,0 +1,25 @@
+//*********** IMPORTS *****************
+var gulp 					= require('gulp'),
+		globbing      = require('gulp-css-globbing'),
+		sass 					= require('gulp-sass'),
+		watch 				= require('gulp-watch');
+
+
+//Gulp compling my Sass files into CSS
+gulp.task('sass', function (){
+	 gulp.src('sass/**/*.scss')
+	 .pipe(globbing({ extensions: ['.scss'] }))
+	 .pipe(sass())
+	 .pipe(gulp.dest('css/')); //save the css to this destination
+});
+
+
+//Gulp performing watch task
+gulp.task('watch',['sass'], function(){
+	gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+//Gulp's default task
+gulp.task('default',['watch']);
+
+
